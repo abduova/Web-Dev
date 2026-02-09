5f264e26db3efe638b2b80ad59d9046398f4bf5d
abduova <a_abduova@kbtu.kz>
2026-02-09 21:54:51 +0500

    lab1

--- Desktop/web-dev/ex30.js
+++ Desktop/web-dev/ex30.js
@@ -1,5 +1,11 @@
-if (i > 5) {
-  alert(i);
-} else {
-  continue;
+for (let i = 0; i < 3; i++) {
+
+  for (let j = 0; j < 3; j++) {
+
+    let input = prompt(`Value at coords (${i},${j})`, '');
+
+    // what if we want to exit from here to Done (below)?
+  }
 }
+
+alert('Done!');
