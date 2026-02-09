e756261dbff2a420c37803adecd9498501cc5403
abduova <a_abduova@kbtu.kz>
2026-02-09 21:50:36 +0500

    lab1

---
+++ Desktop/web-dev/ex24.js
@@ -0,0 +1,24 @@
+let year = prompt('In which year was the ECMAScript-2015 specification published?', '');
+
+if (year < 2015) {
+  alert( 'Too early...' );
+} else if (year > 2015) {
+  alert( 'Too late' );
+} else {
+  alert( 'Exactly!' );
+}
+
+
+
+
+
+
+if (age < 3) {
+  message = 'Hi, baby!';
+} else if (age < 18) {
+  message = 'Hello!';
+} else if (age < 100) {
+  message = 'Greetings!';
+} else {
+  message = 'What an unusual age!';
+}
