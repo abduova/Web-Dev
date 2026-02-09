4265e4d94c69880f668893a2659880bb961855ad
abduova <a_abduova@kbtu.kz>
2026-02-09 21:25:28 +0500

    lab1

---
+++ Desktop/web-dev/lab3/task1/ex13.js
@@ -0,0 +1,6 @@
+alert( 1 / 0 ); // Infinity
+alert( Infinity ); // Infinity
+alert( "not a number" / 2 ); // NaN, such division is erroneous
+alert( NaN + 1 ); // NaN
+alert( 3 * NaN ); // NaN
+alert( "not a number" / 2 - 1 ); // NaN
