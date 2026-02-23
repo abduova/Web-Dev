ba26151b1c93863943c836e1eed303d30f8238ee
abduova <a_abduova@kbtu.kz>
2026-02-23 20:57:20 +0500

    lab4

--- Desktop/web-dev/lab4/task1/src/app/app.ts
+++ Desktop/web-dev/lab4/task1/src/app/app.ts
@@ -1,12 +1,7 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  template: ` Welcome to Angular! `,
 })
-export class App {
-  protected readonly title = signal('task1');
-}
+export class App {}
