2bb104a65ade9255103fc7e4722748b7fada4729
abduova <a_abduova@kbtu.kz>
2026-03-30 15:28:30 +0500

    lab8

--- Desktop/web-dev/lab7/task2/main.py
+++ Desktop/web-dev/lab7/task2/main.py
@@ -1,8 +1,8 @@
 from models import Dog, Cat
 
 
-dog = Dog("Bob", 3, "brown", "Labrador")
-cat = Cat("Milo", 2, "white", 9)
+dog = Dog("Aktos", 3, "white", "Labrador")
+cat = Cat("Murka", 2, "brown", 9)
 
 animals = [dog, cat]
 
